73522aff70f29fd1a39ac1de5cd830540239202f
usuario <usuario@LENOVO-RJ8GRTK4>
2018-03-14 14:19:47 -0600

    Tarea Datos Personales

---
+++ src/datos/generales/DATOSGENERALES.java
@@ -0,0 +1,20 @@
+
+package datos.generales;
+
+/*
+ * @author Daniel Alejandro Orozco Orellana <00200617 at uca.edu.sv>
+ */
+public class DATOSGENERALES 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Nombres: " + "Daniel Alejandro");
+        System.out.println("Apellidos: " + "Orozco Orellana");
+        System.out.println("Carnet: "+"00200617");
+        System.out.println("Edad: "+"18 años");
+        System.out.println("Fecha de nacimiento: "+"14 de julio de 1999");
+        System.out.println("Número de teléfono "+ "7234-9087");
+    }
+    
+}
